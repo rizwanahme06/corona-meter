9d3f0c407b7c0f9e88167891c16a967c212f7c71
rizwanahme06 <96047859+rizwanahme06@users.noreply.github.com>
2022-10-11 17:18:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { csv } from 'd3';
-import  data from '../src/covid_data.csv';
+import data from '../src/csv_file/covid_data.csv';
 import './App.css';
+// import Select from 'react-select';
+
 
 function App() {
+
+  const [country, setcountry] = useState([]);
+  // const [coun,setcoun] = useState( );
+
+
   useEffect(() => {
-    // let i = 0;
-    let arr =[];
-    csv(data, function (data)  {  
-      
-    for (let key in data){
-      arr.push([key,data[key]])
-    }
+    csv(data).then((data) => {
+      // console.log(data);
+      setcountry(data);
     });
-    console.log(arr);
-  }, [])
-  
-  // let i = 0;
-  // csv(data, function (data)  {  
-  //      let arr =[];
-  // for (let key in data){
-  //   arr.push([key,data[key]])
-  // }
-  // console.log(arr);
-  // i++;});
-  
+  }, []);
 
+  let unique =[ ...new Set(country.map((item) => {
+    return item.Country
+  }))];
 
+// let fill = (d)=>{
+//   let sin = d.target.data;
+//   let pro = country.target.Country;
 
+//   if(sin === pro) {
+//     return country.New_cases;
+//   }
+// }
+// console.log(fill);
 
   return (
     <div className="App">
@@ -38,82 +42,52 @@ function App() {
           </h1>
         </header>
         <div className="input-field">
-          <label className='country'>Country: <input type="text" name="country" id="country" /></label>
-          <label className='period'>Period:  <input type="text" name="Period" id='period' /></label>
+          <div className='select'>
+          <h2>Country</h2>
+         <select className='country' >
+          {unique.map((data)=>(
+            <option>{data}</option>
+          ))}
+         </select>
+         </div>
+          <label className='period'>Period:
+            <input type="date" name="Period" id='period' />
+          </label>
         </div>
-        {
-          // data.map((item)=>{
-          //   <div className="result">
-          //   <div className='ncasses'>
-          //     <span className='no_ofcases'>
-          //       New Case:
-          //     </span>
-          //     <div className='reno_cases'>
-          //       {item.New_cases}
-          //     </div>
-          //   </div>
-          //   <div className='tcasses'>
-          //     <span className='to_ofcases'>
-          //       Total Cases:
-          //     </span>
-          //     <div className='reto_cases'>
-
-          //     </div>
-          //   </div>
-          //   <div className='ncasseu'>
-          //     <span className='n_ofcaseu'>
-          //       New Casualties:
-          //     </span>
-          //     <div className='ren_caseu'>
-          //       {item.Cumulative_cases}
-          //     </div>
-          //   </div>
-          //   <div className='tcasseu'>
-          //     <span className='t_ofcaseu'>
-          //       Total Casualties:
-          //     </span>
-          //     <div className='ret_caseu'>
-          //       {}
-          //     </div>
-          //   </div>
-          // </div>
-          // })
-          // JSON.stringify{data};
-        }
         <div className="result">
-        <div className='ncasses'>
-          <span className='no_ofcases'>
-            New Case:
-          </span>
-          <div className='reno_cases'>
-            0
+          <div className='ncasses'>
+            <span className='no_ofcases'>
+              New Case:
+            </span>
+            <div className='reno_cases'>
+              {/* {coun.New_cases} */}
+            </div>
           </div>
-        </div>
-        <div className='tcasses'>
-          <span className='to_ofcases'>
-            Total Cases:
-          </span>
-          <div className='reto_cases'>
+          <div className='tcasses'>
+            <span className='to_ofcases'>
+              Total Cases:
+            </span>
+            <div className='reto_cases'>
 
+            </div>
           </div>
-        </div>
-        <div className='ncasseu'>
-          <span className='n_ofcaseu'>
-            New Casualties:
-          </span>
-          <div className='ren_caseu'>
+          <div className='ncasseu'>
+            <span className='n_ofcaseu'>
+              New Casualties:
+            </span>
+            <div className='ren_caseu'>
 
+            </div>
           </div>
-        </div>
-        <div className='tcasseu'>
-          <span className='t_ofcaseu'>
-            Total Casualties:
-          </span>
-          <div className='ret_caseu'>
+          <div className='tcasseu'>
+            <span className='t_ofcaseu'>
+              Total Casualties:
+            </span>
+            <div className='ret_caseu'>
 
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
